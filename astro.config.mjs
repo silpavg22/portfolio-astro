1b7580a3a2d78b0972c72a29f5020b3db2216e9a
silpavg22 <silpavg22>
2024-02-11 19:36:52 -0600

    added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://silpavg22.github.io'
+  site: 'https://silpavg22.github.io',
+  base: '/my-repo',
 })
