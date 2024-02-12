3f03eed72ce3010df280b749c3e5248ad4278098
silpavg22 <silpavg22>
2024-02-11 22:30:40 -0600

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
+import image from "@astrojs/image";
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [image(),tailwind(), robotsTxt()],
   site: 'https://silpavg22.github.io',
+  base: '/portfolio-astro'
 })
