b149e2cbeb59c8f562014ab739be5dd47a0b66e3
silpavg22 <silpavg22>
2024-02-12 22:15:26 -0600

    gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })],
   site: 'https://silpavg22.github.io',
   base: '/portfolio-astro'
-})
+});
